d54be9b894d9a3f02a3e1e7c428a82b6674f2240
Kristian Hartikainen <kristian.hartikainen@aalto.fi>
2015-10-09 14:56:36 +0300

    Initial implementation of the BlockingEventQueue, unfinished and not tested

--- reactor/reactor/BlockingEventQueue.java
+++ reactor/reactor/BlockingEventQueue.java
@@ -3,34 +3,123 @@ package reactor;
 import reactorapi.BlockingQueue;
 import java.util.List;
 
+// TODO: check the type
 public class BlockingEventQueue<T> implements BlockingQueue<Event<? extends T>> {
+    private List<Event<? extends T>> eventList;
+    private final int capacity;
+
+    // Monitors to be used to wait for the list to be notempty or notFull,
+    // when getting or setting events, respectively.
+    private final Object notEmpty = new Object();
+    private final Object notFull  = new Object();
+
+    private volatile boolean empty = true;
+    private volatile boolean full  = false;
+
     public BlockingEventQueue(int capacity) {
-        // TODO: Implement BlockingEventQueue(int).
+        if (capacity < 1) {
+            throw new IllegalArgumentException("The length of the queue must be > 0");
+        }
+
+        this.capacity = capacity;
+        this.eventList = new List<Event<? extends T>>(capacity);
     }
 
+    // TODO: check the lock!
     public int getSize() {
-        throw new UnsupportedOperationException();
-        // TODO: Implement BlockingEventQueue.getSize().
+        final int size = 0;
+
+        synchronized (eventList) {
+            size = eventList.size();
+        }
+
+        return size;
     }
 
     public int getCapacity() {
-        throw new UnsupportedOperationException(); // Replace this.
-        // TODO: Implement BlockingEventQueue.getCapacity().
+        return this.capacity;
     }
 
+    // TODO: the InterruptedException?
     public Event<? extends T> get() throws InterruptedException {
-        throw new UnsupportedOperationException(); // Replace this.
-        // TODO: Implement BlockingEventQueue.get().
+        Event<? extends T> event;
+
+        synchronized (eventList) {
+            // TODO: Interrupting??
+            try {
+                while (empty) {
+                    notEmpty.wait();
+                }
+            } catch (InterruptedException ie) {
+                // TODO: notEmpty.notify() ?
+                throw ie;
+            }
+
+            event = this.eventList.remove(0);
+
+            if (eventList.size() == capacity - 1) {
+                // TODO: check if notify is sufficient
+                notFull.notifyAll();
+            }
+        }
+
+        return event;
     }
 
-    public synchronized List<Event<? extends T>> getAll() {
-            throw new UnsupportedOperationException(); // Replace this.
-            // TODO: Implement BlockingEventQueue.getAll().
-	}
+    public List<Event<? extends T>> getAll() {
+        synchronized (eventList) {
+            List<Event<? extends T>> eventList;
+        }
+    }
 
     public void put(Event<? extends T> event) throws InterruptedException {
-        // TODO: Implement BlockingEventQueue.put(Event).
+        if (event == null) throw new NullPointerException();
+
+        synchronized (eventList) {
+            try {
+                while (full) {
+                    notFull.wait();
+                }
+            } catch (InterruptedException ie) {
+                throw ie;
+            }
+
+            this.eventList.add(event);
+
+            if (eventList.size() == 1) {
+                // TODO: check if notify is sufficient
+                notEmpty.notifyAll();
+            }
+        }
     }
 
-    // Add other methods and variables here as needed.
+    // private class MySemaphore {
+    //     private int value;
+
+    //     public MySemaphore(int v) {
+    //         if (v < 1) {
+    //             throw new IllegalArgumentException("Semaphore size must be > 0");
+    //         }
+
+    //         this.value = v;
+    //     }
+
+    //     public synchronized void release() {
+    //         this.value++;
+    //         this.notify();
+    //     }
+
+    //     public synchronized void acquire() throws InterruptedException {
+    //         try {
+    //             while(this.value <= 0) {
+    //                 this.wait();
+    //             }
+    //         } catch (InterruptedException ie) {
+    //             // TODO: should something else be done here?
+    //             throw ie;
+    //         }
+
+    //         this.value--;
+    //     }
+    // }
 }
