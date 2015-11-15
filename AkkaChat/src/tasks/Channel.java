e9c49837b99772f497d8e8ac1ae5bd56cf14022d
Kristian Hartikainen <kristian.hartikainen@aalto.fi>
2015-11-15 19:07:11 +0200

    Initial Channel and ChannelManager implementations

--- AkkaChat/src/tasks/Channel.java
+++ AkkaChat/src/tasks/Channel.java
@@ -1,9 +1,14 @@
 package tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import messages.*;
 import akka.actor.*;
 
 public class Channel extends UntypedActor {
+    final List<ChatMessage> messageHistory = new ArrayList<ChatMessage>();
+    final List<ActorRef> users = new ArrayList<ActorRef>();
 
     private String getChannelName() {
         return self().path().name();
@@ -11,14 +16,35 @@ public class Channel extends UntypedActor {
 
     @Override
     public void onReceive(Object msg) throws Exception {
-        // TODO: Implement the required functionality.
+        // TODO: Check the functionality
         if (msg instanceof ChatMessage) {
+            final ChatMessage newMessage = (ChatMessage)msg;
+            final Backlog message = new Backlog(getChannelName(), newMessage);
 
+            messageHistory.add(newMessage);
+
+            for (ActorRef user : users) {
+                user.tell(message, self());
+            }
         } else if (msg instanceof AddUser) {
+            final ActorRef user = ((AddUser)msg).user;
+            final Backlog history = new Backlog(getChannelName(), messageHistory);
 
+            context().watch(user);
+            users.add(user);
+            user.tell(history, self());
+            user.tell(new UserAdded(getChannelName(), self()), self());
         } else if (msg instanceof RemoveUser) {
+            final ActorRef user = ((RemoveUser)msg).user;
 
-        } else
+            users.remove(user);
+            user.tell(new UserRemoved(getChannelName(), self()), self());
+        } else if (msg instanceof Terminated) {
+            final Terminated terminated = (Terminated)msg;
+
+            users.remove(terminated.getActor());
+        } else {
             unhandled(msg);
+        }
     }
 }
--- AkkaChat/src/tasks/ChannelManager.java
+++ AkkaChat/src/tasks/ChannelManager.java
@@ -1,16 +1,32 @@
 package tasks;
 
+import java.util.HashMap;
+
 import messages.*;
 import akka.actor.*;
 
 public class ChannelManager extends UntypedActor {
+    private final HashMap<String, ActorRef> channelMap =
+        new HashMap<String, ActorRef>();
 
     @Override
     public void onReceive(Object msg) throws Exception {
-        // TODO: Implement the required functionality.
+        // TODO: Check the functionality
+    	// TODO: Watch the channels?
         if (msg instanceof GetOrCreateChannel) {
+            final String name = ((GetOrCreateChannel)msg).name;
+            // TODO: should channelActor be final?
+            ActorRef channelActor = channelMap.get(name);
 
-        } else
+            if (channelActor == null) {
+                channelActor = context()
+                    .actorOf(Props.create(Channel.class), name);
+                channelMap.put(name, channelActor);
+            }
+
+            getSender().tell(channelActor, getSelf());
+        } else {
             unhandled(msg);
+        }
     }
 }
